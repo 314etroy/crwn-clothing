a84508bd4fbd10a85c1dde072e84f4dc9904cf39
314etroy <gtx7703d@gmail,com>
2020-12-27 17:25:33 +0200

    I implement CSX in Project

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,34 +1,27 @@
 import React from 'react';
-import CustomButton from '../custom-button/custom-button.component';
 import { connect } from 'react-redux';
 import { addItem } from '../../redux/cart/cart.actions';
-import './collection-item.styles.scss';
+import { CollectionItemContainer, AddButton, BackgroundImage, CollectionFooterContainer, NameContainer, PriceContainer } from './collection-styles.styles';
 
 const CollectionItem = ({ item, addItem }) => {
 
     const { name, price, imageUrl } = item;
 
     return (
-        <div className='collection-item'>
+        <CollectionItemContainer>
 
-            <div
-                className='image'
-                style={{
-                    backgroundImage: `url(${imageUrl})`
-                }}
-            >
-            </div>
+            <BackgroundImage className='image' imageUrl={imageUrl}/>
 
-            <div className='collection-footer'>
-                <span className='name'>{name}</span>
-                <span className='price'>{price}</span>
-            </div>
+            <CollectionFooterContainer>
+                <NameContainer>{name}</NameContainer>
+                <PriceContainer>{price}</PriceContainer>
+            </CollectionFooterContainer>
 
-            <CustomButton onClick={() => addItem(item)} inverted>
+            <AddButton onClick={() => addItem(item)} inverted>
                 Add to cart
-            </CustomButton>
+            </AddButton>
 
-        </div>
+        </CollectionItemContainer>
     );
 
 };
@@ -41,6 +34,39 @@ export default connect(
     null,
     mapDispatchToProps
 )(CollectionItem);
+
+// import './collection-item.styles.scss';
+
+// const CollectionItem = ({ item, addItem }) => {
+
+//     const { name, price, imageUrl } = item;
+
+//     return (
+//         <div className='collection-item'>
+
+//             <div
+//                 className='image'
+//                 style={{
+//                     backgroundImage: `url(${imageUrl})`
+//                 }}
+//             >
+//             </div>
+
+//             <div className='collection-footer'>
+//                 <span className='name'>{name}</span>
+//                 <span className='price'>{price}</span>
+//             </div>
+
+//             <CustomButton onClick={() => addItem(item)} inverted>
+//                 Add to cart
+//             </CustomButton>
+
+//         </div>
+//     );
+
+// };
+
+// - - - -
 
 // const CollectionItem = ({ id, name, price, imageUrl, addItem }) => (
 //     <div className='collection-item'>
